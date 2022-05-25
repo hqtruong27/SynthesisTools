ea46423ecc9e4f1857cd035535f0419a52ace078
Truong Hoang <hqtruong27@gmail.com>
2022-05-25 19:08:56 +0700

    test

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -1,5 +1,5 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+//using Microsoft.AspNetCore.Components;
+//using Microsoft.AspNetCore.Components.Web;
 using WebServer.IocConfig;
 
 var builder = WebApplication.CreateBuilder(args);
